d41fa8d46b4544d3f3efb0ba4ec1fd1fea92fa4f
Ayed Oussama <ayed.oussama@gmail.com>
2017-03-13 11:20:17 +0100

    cardboard&GearVR commit
    unity project

---
+++ Assets/LoadImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadImage : MonoBehaviour
+{
+
+
+    public string url = "http://www.roadtovr.com/wp-content/uploads/2014/09/Venice.Still001.jpeg";
+    Texture2D img;
+    public GameObject sphere;
+    // Use this for initialization
+    void Start()
+    {
+
+        StartCoroutine(LoadImg());
+    }
+    IEnumerator LoadImg()
+    {
+
+        yield return 0;
+        WWW imgLink = new WWW(url);
+        yield return imgLink;
+        img = imgLink.texture;
+        sphere.GetComponent<Renderer>().material.mainTexture = img;
+
+    }
+    // Update is called once per frame
+
+}
